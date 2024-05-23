4247aaa0d042c36263be74c1b75e891a402fe4dd
Sam R <sam.rompas@gmail.com>
2024-05-23 11:26:06 +0200

    Update data.js

--- static/data.js
+++ static/data.js
@@ -4,64 +4,48 @@ const COUNTRY_DATA = {
             "name": "Orang Sawah Villa (Ubud)",
             "latlng": [-8.457363948, 115.2616381],
             "pictures": [
-                "/static/homepage/images/indonesia/ubud1.jpeg",
-                "/static/homepage/images/indonesia/ubud2.jpeg",
-                "/static/homepage/images/indonesia/ubud3.jpeg"
+                "/static/images/indonesia/ubud1.webp",
+                "/static/images/indonesia/ubud2.webp",
+                "/static/images/indonesia/ubud3.webp"
             ]
         }, "indo2": {
             "name": "Balijana Bungalow (Lovina)",
             "latlng": [-8.166686231, 115.030395],
             "pictures": [
-                "/static/homepage/images/indonesia/lovina1.png",
-                "/static/homepage/images/indonesia/lovina2.JPG",
-                "/static/homepage/images/indonesia/lovina4.jpeg"
+                "/static/images/indonesia/lovina1.webp",
+                "/static/images/indonesia/lovina2.webp",
+                "/static/images/indonesia/lovina3.webp"
             ]
         }, "indo3": {
             "name": "The Local Entikentikan (Bangli)",
             "latlng": [-8.433213224, 115.3740159],
             "pictures": [
+                "/static/images/indonesia/bangli1.webp",
                 "",
-                "",
-                "/static/homepage/images/indonesia/bangli1.jpeg"
+                ""
             ]
         }, "indo4": {
             "name": "Jatiluwih Rice Terraces (onderweg)",
             "latlng": [-8.361928371, 115.1321663],
             "pictures": [
+                "/static/images/indonesia/jatiluwih1.webp",
                 "",
-                "",
-                "/static/homepage/images/indonesia/jatiluwih1.jpeg"
+                ""
             ]
         }, "indo5": {
             "name": "Ulun Danu Beratan Temple (onderweg)",
             "latlng": [-8.26614186, 115.167489],
             "pictures": [
+                "/static/images/indonesia/beratan1.webp",
                 "",
-                "",
-                "/static/homepage/images/indonesia/beratan1.JPG"
+                ""
             ]
         }, "indo6": {
             "name": "Olympus Coffee Bali (onderweg)",
             "latlng": [-8.272835968, 115.3475925],
             "pictures": [
-                "/static/homepage/images/indonesia/olympuscoffee1.jpeg",
-                "",
-                "/static/homepage/images/indonesia/olympuscoffee2.jpeg"
-            ]
-        }, "indo7": {
-            "name": "Porkys (Manado)",
-            "latlng": [1.478120761, 124.8334848],
-            "pictures": [
-                "",
-                "",
-                ""
-            ]
-        }, "indo8": {
-            "name": "S Loft Manado Hotel (Manado)",
-            "latlng": [1.466985529, 124.8395536],
-            "pictures": [
-                "",
-                "",
+                "/static/images/indonesia/olympuscoffee1.webp",
+                "/static/images/indonesia/olympuscoffee2.webp",
                 ""
             ]
         }
